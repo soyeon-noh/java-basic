74f7597382a0bfd886a7b729f4431df595efcbad
soyeon-noh <1010qwerty12@gmail.com>
2024-11-22 20:25:45 +0900

    9. final

--- src/final1/ConstructInit.java
+++ src/final1/ConstructInit.java
@@ -1,5 +1,6 @@
 package final1;
 
+// final 멤버변수를 생성자를 통해 초기화
 public class ConstructInit {
 
     // final 을 필드에 사용하는 경우
--- src/final1/FieldInit.java
+++ src/final1/FieldInit.java
@@ -1,9 +1,15 @@
 package final1;
 
+// final 멤버변수를 필드에서 초기화
 public class FieldInit {
 
-    // static final 이 붙으면 대문자로 쓰는 관습
+    // static + final 이 붙으면 대문자로 쓰는 관습
     static final int CONST_VALUE = 10;
     final int value = 10;
 
+
+    // 이미 초기화 되어 있기 때문에 생성자를 통해 값 할당 X
+//    public FieldInit(int value) {
+//        this.value = value;
+//    }
 }
